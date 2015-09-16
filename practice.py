254c3d40af41799ba671151dddbe7335663cc13e
saurabh <you@example.com>
2015-09-17 02:34:53 +0530

    upper bounded

--- practice.py
+++ practice.py
@@ -2,7 +2,6 @@ import sys, pygame
 import time
 from sdl2 import *
 
-
 pygame.init()
 size = width, height = 1280, 760
 movedown = [0, 10]
@@ -19,34 +18,30 @@ eagle4 = pygame.image.load("resources/4.gif")
 
 eaglerect = eagle1.get_rect()
 
-i=0
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    if event.type == pygame.KEYDOWN:
+  if event.type == pygame.KEYDOWN:
 		if event.key == pygame.K_UP:
-			eaglerect = eaglerect.move(moveup)
+			if eaglerect.centery>=(eaglerect.height/2):
+				eaglerect = eaglerect.move(moveup)
 		if event.key == pygame.K_DOWN:
-			eaglerect = eaglerect.move(movedown)
-    i=i+1
-    image_delay=10
+			if eaglerect.centery <= 760-(eaglerect.height):
+				eaglerect = eaglerect.move(movedown)
+
+
+
     screen.fill(black)
-    if i<image_delay:
-        screen.blit(eagle1, eaglerect)
-        time.sleep(0.025)
-        pygame.display.flip()
-    if (i>image_delay-1 and i<2*image_delay):
-        screen.blit(eagle2, eaglerect)
-        time.sleep(0.025)
-        pygame.display.flip()
-    if (i>2*image_delay-1 and i< 3*image_delay):
-        screen.blit(eagle3, eaglerect)
-        time.sleep(0.025)
-        pygame.display.flip()
-    if (i>3*image_delay-1 and i<4*image_delay):
-        screen.blit(eagle4, eaglerect)
-        time.sleep(0.025)
-        pygame.display.flip()
-        if i==4*image_delay-1:
-            i=0
+    screen.blit(eagle1, eaglerect)
+    time.sleep(0.025)
+    pygame.display.flip()
+    screen.blit(eagle2, eaglerect)
+    time.sleep(0.025)
+    pygame.display.flip()
+    screen.blit(eagle3, eaglerect)
+    time.sleep(0.025)
+    pygame.display.flip()
+    screen.blit(eagle4, eaglerect)
+    time.sleep(0.025)
+    pygame.display.flip()
