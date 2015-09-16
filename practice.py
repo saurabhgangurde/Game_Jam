325eb4bf72e0418e96e5a9f04b2741d268a48637
saurabh <you@example.com>
2015-09-17 02:02:12 +0530

    eagle flagmovement

--- practice.py
+++ practice.py
@@ -1,8 +1,11 @@
 import sys, pygame
 import time
+from sdl2 import *
+
 pygame.init()
 size = width, height = 1280, 760
-speed = [10, 0]
+movedown = [0, 10]
+moveup = [0, -10]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
@@ -19,20 +22,28 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    eaglerect = eaglerect.move(speed)
+    if event.type == pygame.KEYDOWN:
+		if event.key == pygame.K_UP:
+			eaglerect = eaglerect.move(moveup)
+		if event.key == pygame.K_DOWN:
+			eaglerect = eaglerect.move(movedown)
+
+
+   	
+
     
 
 
     screen.fill(black)
     screen.blit(eagle1, eaglerect)
-    time.sleep(0.1)
+    time.sleep(0.025)
     pygame.display.flip()
     screen.blit(eagle2, eaglerect)
-    time.sleep(0.1)
+    time.sleep(0.025)
     pygame.display.flip()
     screen.blit(eagle3, eaglerect)
-    time.sleep(0.1)
+    time.sleep(0.025)
     pygame.display.flip()
     screen.blit(eagle4, eaglerect)
-    time.sleep(0.1)
+    time.sleep(0.025)
     pygame.display.flip()
